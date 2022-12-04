e6d5ef62f42ffb0fc7bb70e985fda2918d962ca7
acclifford <115430341+acclifford@users.noreply.github.com>
2022-12-04 14:53:40 -0500

    trying again in a new project - taking it step by step

---
+++ src/FleetManagement.java
@@ -0,0 +1,103 @@
+import java.util.*;
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+public class FleetManagement {
+
+    public static final Scanner keyboard = new Scanner(System.in);
+    public static final int MAX_BOAT_LENGTH = 100;
+    public static final double MAX_PURCHASE_PRICE = 1000000;
+
+    enum type {POWER, SAILING}
+
+    private ArrayList<Boat> fleet;
+
+    //-----------------------------------------------------------------------
+
+    public static void main(String[] args) {
+
+        ArrayList<Boat>fleet = new ArrayList<Boat>();
+        String path = "C:\\Users\\Ailis\\Desktop\\CSC120_LAB\\FleetData.csv";
+        initFromCSVFile(path, fleet);
+
+        menu(fleet);
+
+    }
+    //-----------------------------------------------------------------------
+
+    private static ArrayList<Boat> initFromCSVFile(String path, ArrayList<Boat>fleet) {
+
+        String line = "";
+        Boat newBoat = new Boat();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                newBoat = createBoat(values);
+                fleet.add(newBoat);
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return(fleet);
+    }
+    //-----------------------------------------------------------------------
+    public static Boat createBoat(String[] attributes){
+
+        type theType;
+
+        theType = type.valueOf(attributes[0]);
+        String name = attributes[1];
+        int manufacturer = Integer.parseInt(attributes[2]);
+        String makeAndModel = attributes[3];
+        int length = Integer.parseInt(attributes[4]);
+        double purchasePrice = Double.parseDouble(attributes[5]);
+
+        return new Boat (theType,name,manufacturer,makeAndModel,length,purchasePrice);
+    }
+    //-----------------------------------------------------------------------
+    private static void menu(ArrayList<Boat>fleet) {
+
+        char menuSelection;
+        int index;
+        int index2 = 0;
+        Boat myBoat = new Boat();
+        String boatName;
+        double newExpense;
+
+
+        System.out.println("Welcome to the Fleet Management System");
+        System.out.println("---------------------------------------");
+
+        System.out.print("(P)rint, (A)dd, (R)emove, (E)xpense, e(X)it : ");
+        menuSelection = Character.toUpperCase(keyboard.next().charAt(0));
+
+        do{
+            switch(menuSelection){
+                case 'P':
+                        break;
+                case 'A':
+                        break;
+                case 'R':
+                        break;
+                case 'E':
+                        break;
+                case 'X':
+                        System.out.println(" ");
+                        System.out.println("Exiting the Fleet Management System");
+                        break;
+            }
+        } while(menuSelection != 'X');
+
+    }
+    //-----------------------------------------------------------------------
+}
